faa64291cd148ad0ade9667135cf3f1ccf093fe0
Julie <julienilsen.l@gmail.com>
2021-02-10 23:31:31 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import GoalList from './components/GoalList';
+import Awais from './components/frame.svg';
 
 function App() {
   return (
     <div className="course-goals">
       <h2>Kurs mål</h2>
+      <img scr={Awais} alt="QR code" />
+      <img scr="/components/frame.svg" alt="QR code" />
       <GoalList />
       
      
--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './GoalList.css';
+import frame from './frame.png';
+
+
 
 const GoalList = () => {
 
     return ( 
-    <ul className="goal">
-        <li>Finish the course</li>
-        <li>Learn all about the course</li>
-        <li>Help others</li>
-    </ul>
+    
+        <img scr={frame} alt="QR code" />
+    
     );
 };
 
